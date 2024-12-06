4512eca2cefad7a3408b9c7dba9d11e908700fa8
alenzo <alenzo.amico@gmail.com>
2024-12-06 02:37:42 +0100

    add rope at 2:30 am

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+const path = require('path');
+
+// Middleware pour traiter les données de formulaire
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Définir le dossier contenant les fichiers statiques
+app.use(express.static(path.join(__dirname)));
 
 // Définir un point de route simple
 app.get('/', (req, res) => {
-    res.send('Bonjour, votre application Node.js est maintenant une application web !');
+    res.sendFile(path.join(__dirname, 'templates/index.html'));
 });
 
 // Démarrer le serveur sur le port 3000
